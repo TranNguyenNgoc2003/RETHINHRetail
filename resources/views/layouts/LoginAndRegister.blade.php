5c248d9c20d50e75bb00efdc09af2a049287b1ae
TranNguyenNgoc2003 <84257356+TranNguyenNgoc2003@users.noreply.github.com>
2025-02-03 04:23:49 +0700

    Fix Name Store

--- resources/views/layouts/LoginAndRegister.blade.php
+++ resources/views/layouts/LoginAndRegister.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>TechStore</title>
+    <title>RETHINK Retail</title>
 
     <!-- Font Icon -->
     <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
@@ -21,5 +21,5 @@
     <!-- JS -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="js/main.js"></script>
-</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</body>
 </html>
