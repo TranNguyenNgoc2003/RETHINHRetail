378ca08aeb7ce49440f136bfeffca380adaea2ed
TranNguyenNgoc2003 <84257356+TranNguyenNgoc2003@users.noreply.github.com>
2025-02-25 08:00:13 +0700

    RENAME FILE

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginRegisterController;
+use App\Http\Controllers\Auth\LoginAndRegisterController;
 use App\Http\Controllers\Auth\SocialLogin;
 use App\Http\Controllers\Auth\SocialLoginCallback;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(LoginRegisterController::class)->group(function () {
+Route::controller(LoginAndRegisterController::class)->group(function () {
     Route::get('/register', 'register')->name('register');
     Route::post('/registerAuth', 'registerAuth')->name('registerAuth');
     Route::get('/login', 'login')->name('login');
