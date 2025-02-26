f3de77b86710fdc52b9cf3c6722c342a84a08108
TranNguyenNgoc2003 <84257356+TranNguyenNgoc2003@users.noreply.github.com>
2025-02-26 16:24:19 +0700

    fix scss home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -120,37 +120,39 @@
         <div class="row product__row">
             @foreach ($products->sortByDesc('rating')->take(8) as $product)
                 <div class="col-md-3 product__col">
-                    <a href="{{--{{ route('product.detail', ['id' => $product->id]) }}--}}" class="product__card">
-                        @if ($product->discount)
-                            <span class="product__card--badge">Giảm {{ $product->discount }}%</span>
-                        @endif
-                        <div class="product__card--image">
-                            <img alt="{{ $product->name_product }}"
-                                src="{{ asset('images/Product/' . $product->images->first()->path_img) }}">
-                        </div>
-                        <div class="product__card--content">
-                            <div class="product__card--content-title">{{ $product->name_product }}</div>
-                            <div class="product__card--content-rating rating">
-                                <div class="rating__star">
-                                    @for ($i = 1; $i <= 5; $i++)
-                                        <i class="fas fa-star {{ $i <= $product->rating ? 'text-warning' : 'text-secondary' }}"></i>
-                                    @endfor
+                    <div class="product__card">
+                        <a href="{{--{{ route('product.detail', ['id' => $product->id]) }}--}}">
+                            @if ($product->discount)
+                                <span class="product__card--badge">Giảm {{ $product->discount }}%</span>
+                            @endif
+                            <div class="product__card--image">
+                                <img alt="{{ $product->name_product }}"
+                                    src="{{ asset('images/Product/' . $product->images->first()->path_img) }}">
+                            </div>
+                            <div class="product__card--content">
+                                <div class="product__card--content-title">{{ $product->name_product }}</div>
+                                <div class="product__card--content-rating rating">
+                                    <div class="rating__star">
+                                        @for ($i = 1; $i <= 5; $i++)
+                                            <i class="fas fa-star {{ $i <= $product->rating ? 'text-warning' : 'text-secondary' }}"></i>
+                                        @endfor
+                                    </div>
+                                    <small class="rating__count">({{ $product->rating }}/5)</small>
                                 </div>
-                                <small class="rating__count">({{ $product->rating }}/5)</small>
+                                <p class="product__card--content-description">{{ $product->description }}</p>
+                                <div class="product__card--content-priceInfo priceInfo">
+                                    <span class="priceInfo__priceCurrent">
+                                        {{ number_format(round($product->price - ($product->price / 100) * $product->discount, -3), 0, ',', '.') }}
+                                        VND
+                                    </span>
+                                    @if ($product->price && $product->discount)
+                                        <span class="priceInfo__priceUnline">{{ number_format($product->price, 0, ',', '.') }}
+                                            VND</span>
+                                    @endif
+                                </div>
                             </div>
-                            <p class="product__card--content-description">{{ $product->description }}</p>
-                            <div class="product__card--content-priceInfo priceInfo">
-                                <span class="priceInfo__priceCurrent">
-                                    {{ number_format(round($product->price - ($product->price / 100) * $product->discount, -3), 0, ',', '.') }}
-                                    VND
-                                </span>
-                                @if ($product->price && $product->discount)
-                                    <span class="priceInfo__priceUnline">{{ number_format($product->price, 0, ',', '.') }}
-                                        VND</span>
-                                @endif
-                            </div>
-                        </div>
-                    </a>
+                        </a>
+                    </div>
                 </div>
             @endforeach
         </div>
