19b88c1a2c8f3ab58d2233b48223dfa4235f5a27
TranNguyenNgoc2003 <84257356+TranNguyenNgoc2003@users.noreply.github.com>
2025-03-11 16:05:49 +0700

    fix model

--- app/Models/Delivery.php
+++ app/Models/Delivery.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Delivery extends Model
 {
+    use HasFactory;
+
+    protected $table = 'deliveries';
+
     protected $fillable = [
         'fullname',
         'address',
--- app/Models/DetailOrder.php
+++ app/Models/DetailOrder.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DetailOrder extends Model
 {
+    use HasFactory;
+    
     protected $table = 'detail_orders';
 
     protected $fillable = [
